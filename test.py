102de64e431668d5d69d150ec4806b86e84cc14d
Monishwaran <monishwaran@macbook.local.com>
2022-01-17 12:10:20 -0800

    added female voices

---
+++ test.py
@@ -0,0 +1,9 @@
+import pyttsx3
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+voiceFemales = filter(lambda v: v.gender == 'VoiceGenderFemale', voices)
+for v in voiceFemales:
+    engine.setProperty('voice', v.id)
+    print(v.id)
+    engine.say('Hello world from ' + v.name)
+    engine.runAndWait()
